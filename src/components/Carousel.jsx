257956665309ca98c8d64d49a1f9e6fa240fcb74
rekawijaya <rekawijaya27@gmail.com>
2024-04-21 00:36:19 +0700

    revisi all bugs done in home page

--- src/components/Carousel.jsx
+++ src/components/Carousel.jsx
@@ -12,74 +12,90 @@ function Carousel() {
     dots: false,
     infinite: true,
     speed: 500,
-    slidesToShow: 2.5,
+    slidesToShow: 2.3,
     slidesToScroll: 1,
     autoplay: true,
     autoplaySpeed: 1500,
     arrows: false,
+    draggable: false,
+    pauseOnHover: false,
   };
   return (
-    <div className="w-full h-full -mt-50">
-      <div className=" h-auto cover-size">
-        <Slider {...settings}>
-          <div className="m-auto mt-40 ">
-            <div className="bg-white rounded-xl card-size border-gray-900 border-2 shadow-md">
-              <div className="rounded-full w-16 h-16 -mt-9 ml-5">
-                <img className="w-16 h-16" src={Mascot2} alt="" />
-              </div>
-              <div className="">
-                <h4 className="headline-4 ml-6 mt-3">Nailah Farah Fadhilah</h4>
-                <p className="paragraph mx-6 pb-4 border-b border-black">
-                  Member group of New Orbit
-                </p>
-                <p className="paragraph ml-6">
-                  “Aku sangat senang, seru banget bisa kenal sama kakak-kakak di
-                  Skyshare Academy dan dapet mentor yang alhamdulillah cocok dan
-                  bisa main bareng teman-teman sefrekuensi, nambah ilmu, dan
-                  buat aku tetap menjaga apa yang harus jaga.”
-                </p>
-              </div>
-            </div>
-          </div>
-          <div className="m-auto mt-40">
-            <div className="bg-white card-size rounded-xl border-gray-900 border-2 shadow-md">
-              <div className="rounded-full w-16 h-16 -mt-9 ml-5">
-                <img className="w-16 h-16" src={Mascot4} alt="" />
-              </div>
-              <div className="">
-                <h4 className="headline-4 ml-6 mt-3">Bunda Annisa Rahmawati</h4>
-                <p className="paragraph mx-6 pb-4 border-b border-black">
-                  Orang tua dari Ananda Mecca
-                </p>
-                <p className="paragraph ml-6">
-                  ”Percaya banget deh pokoknya sama Skyshare Academy”
-                </p>
+    <div className="h-full">
+      <div className="flex absolute width z-40  width2 -ml-3 justify-between">
+        <h1 className=" font-thin slash3">/</h1>
+        <h1 className=" font-thin slash4">\</h1>
+      </div>
+      <div className="  h-full -mt-50">
+        <div className=" pr-16 cover-size ">
+          <Slider {...settings}>
+            <div className=" m-auto mt-40 ">
+              <div className="bg-white rounded-xl card-size border-gray-900 border-2 shadow-md">
+                <div className="rounded-full w-16 h-16 -mt-9 ml-5">
+                  <img className="w-16 h-16" src={Mascot2} alt="" />
+                </div>
+                <div className="">
+                  <h4 className="headline-4 ml-6 mt-3">
+                    Nailah Farah Fadhilah
+                  </h4>
+                  <p className="paragraph mx-6 pb-4 border-b border-black">
+                    Member group of New Orbit
+                  </p>
+                  <p className="paragraph ml-6">
+                    “Aku sangat senang, seru banget bisa kenal sama kakak-kakak
+                    di Skyshare Academy dan dapet mentor yang alhamdulillah
+                    cocok dan bisa main bareng teman-teman sefrekuensi, nambah
+                    ilmu, dan buat aku tetap menjaga apa yang harus jaga.”
+                  </p>
+                </div>
               </div>
             </div>
-          </div>
-          <div className="m-auto mt-40">
-            <div className="bg-white card-size rounded-xl border-gray-900 border-2 shadow-md">
-              <div className="rounded-full w-16 h-16 -mt-9 ml-5">
-                <img className="w-16 h-16" src={Mascot3} alt="" />
-              </div>
-              <div className="">
-                <h4 className="headline-4 ml-6 mt-3">
-                  Muhammad Naufal Laksita
-                </h4>
-                <p className="paragraph mx-6 pb-4 border-b border-black">
-                  Member group of Legacy
-                </p>
-                <p className="paragraph ml-6">
-                  “Setelah mengikuti MSIM Mentoring Consulting terutama program
-                  Skyshare Academy saya merasa memiliki teman yang satu
-                  frekuensi. Teman-teman di group bukan hanya menjadi partner
-                  untuk saling berbagi cerita namun juga sebagai penjaga
-                  keimanan serta kesadaran saya sebagai manusia. ”
-                </p>
+            <div className="m-auto mt-40">
+              <div className="bg-white card-size rounded-xl border-gray-900 border-2 shadow-md">
+                <div className="rounded-full w-16 h-16 -mt-9 ml-5">
+                  <img className="w-16 h-16" src={Mascot4} alt="" />
+                </div>
+                <div className="">
+                  <h4 className="headline-4 ml-6 mt-3">
+                    Bunda Annisa Rahmawati
+                  </h4>
+                  <p className="paragraph mx-6 pb-4 border-b border-black">
+                    Orang tua dari Ananda Mecca
+                  </p>
+                  <p className="paragraph ml-6">
+                    ”Percaya banget deh pokoknya sama Skyshare Academy”
+                  </p>
+                </div>
               </div>
             </div>
-          </div>
-        </Slider>
+            <div className="m-auto mt-40">
+              <div className="bg-white card-size rounded-xl border-gray-900 border-2 shadow-md">
+                <div className="rounded-full w-16 h-16 -mt-9 ml-5">
+                  <img className="w-16 h-16" src={Mascot3} alt="" />
+                </div>
+                <div className="">
+                  <h4 className="headline-4 ml-6 mt-3">
+                    Muhammad Naufal Laksita
+                  </h4>
+                  <p className="paragraph mx-6 pb-4 border-b border-black">
+                    Member group of Legacy
+                  </p>
+                  <p className="paragraph ml-6">
+                    “Setelah mengikuti MSIM Mentoring Consulting terutama
+                    program Skyshare Academy saya merasa memiliki teman yang
+                    satu frekuensi. Teman-teman di group bukan hanya menjadi
+                    partner untuk saling berbagi cerita namun juga sebagai
+                    penjaga keimanan serta kesadaran saya sebagai manusia. ”
+                  </p>
+                </div>
+              </div>
+            </div>
+          </Slider>
+        </div>
+        <div className="flex absolute width z-40 -ml-3 justify-between">
+          <h1 className=" font-thin slash1">\</h1>
+          <h1 className=" font-thin slash2">/</h1>
+        </div>
       </div>
     </div>
   );
--- src/components/Testimoni.jsx
+++ src/components/Testimoni.jsx
@@ -5,21 +5,23 @@ import Carousel from "./Carousel";
 function Testimoni() {
   return (
     <>
-      <div className="testimoni flex">
-        <div className="flex h-3/4 mt-24 w-2/5 border-r-2 border-black items-center">
-          <div className="w-96 -mt-20 ml-32">
-            <img className="mascot" src={Mascot1} alt="mascot" />
-            <h1 className="headline-2">🔥 Apa Kata Mereka?</h1>
-            <p className="paragraph mt-4">
-              Simakk Yukk apa kata para alumni & orang tua pelajar tentang
-              program dari MSiM!
-            </p>
+      <div className="testimoni flex justify-center">
+        <div className="flex max-w-6xl">
+          <div className="flex  h-3/4 mt-24 w-2/5 border-r-2 border-black items-center">
+            <div className="w-96 -mt-20">
+              <img className="mascot" src={Mascot1} alt="mascot" />
+              <h1 className="headline-2">🔥 Apa Kata Mereka?</h1>
+              <p className="paragraph mt-4">
+                Simakk Yukk apa kata para alumni & orang tua pelajar tentang
+                program dari MSiM!
+              </p>
+            </div>
           </div>
+          <div className="   w-3/5">
+            <Carousel />
+          </div>
+          <div className=" w-1/12 h-3/4 border-l-2 bg-testimoni border-black mt-24 ml-1  z-10"></div>
         </div>
-        <div className="  w-3/5">
-          <Carousel />
-        </div>
-        <div className=" w-1/12 h-3/4 border-l-2 bg-testimoni border-black mt-24 -ml-20  z-10"></div>
       </div>
     </>
   );
