9b61f50804468d78e63f9a6ac5cc0e35ba7b3cd6
nhsltn <nhsltndev@gmail.com>
2024-04-20 20:42:52 +0700

    bug fixing revisi ui homepage

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -3,7 +3,7 @@ import React from "react";
 function Button({ children, onClick }) {
   return (
     <button
-      className=" text-white py-4 px-6 font-bold flex items-center"
+      className=" text-white py-4 px-6 font-bold flex items-center button"
       onClick={onClick}
     >
       <span className="mr-2">{children}</span>
