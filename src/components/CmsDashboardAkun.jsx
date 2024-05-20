cdb5c4edf453f600855b83376d9826b3f1ab6fdb
WeissCurry <Maulanasykari@gmail.com>
2024-05-21 00:30:03 +0700

    changing endpoint CMS

--- src/components/CmsDashboardAkun.jsx
+++ src/components/CmsDashboardAkun.jsx
@@ -23,25 +23,25 @@ function CmsDashboardAkun() {
                   </Link>
                 </li>
                 <li className=" py-4 w-64 px-4">
-                  <Link className="flex gap-4" to="/">
+                  <Link className="flex gap-4" to="/ourprogram/talent">
                     <img className="w-6" src={ArrowRight} alt="" />
                     <p className=" text-base">Talent Academy</p>
                   </Link>
                 </li>
                 <li className="  py-4 w-64 px-4">
-                  <Link className="flex gap-4" to="/">
+                  <Link className="flex gap-4" to="/ourprogram/mentor">
                     <img className="w-6" src={ArrowRight} alt="" />
                     <p className=" text-base">Mentor Academy</p>
                   </Link>
                 </li>
                 <li className=" py-4 w-64 px-4">
-                  <Link className="flex gap-4" to="/">
-                    <img className="w-6" src={ArrowRight} alt="" />
+                  <Link className="flex gap-4" to="">
+                    <img className="w-6" src={ArrowRight} alt="/ourprogram/parent" />
                     <p className=" text-base">Parents Academy</p>
                   </Link>
                 </li>
                 <li className=" py-4 w-64 px-4">
-                  <Link className="flex gap-4" to="/">
+                  <Link className="flex gap-4" to="/article">
                     <img className="w-6" src={Edit} alt="" />
                     <p className=" text-base">Article</p>
                   </Link>
