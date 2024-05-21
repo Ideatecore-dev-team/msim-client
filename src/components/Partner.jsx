37ab75707d434cdb783261447c84d35086ce77ab
nhsltn <nhsltndev@gmail.com>
2024-05-22 00:23:18 +0700

    Finishing Homepage Responsive

--- src/components/Partner.jsx
+++ src/components/Partner.jsx
@@ -1,9 +1,33 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import Slider from "react-slick";
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
+import "./Partner.css";
 import Partner1 from "../../public/images/partner1.png";
 import Partner2 from "../../public/images/partner2.png";
 import Partner3 from "../../public/images/partner3.png";
 
 function Partner() {
+  const [isMobile, setIsMobile] = useState(window.innerWidth <= 639);
+
+  useEffect(() => {
+    const handleResize = () => {
+      setIsMobile(window.innerWidth <= 639);
+    };
+
+    window.addEventListener("resize", handleResize);
+    return () => window.removeEventListener("resize", handleResize);
+  }, []);
+
+  const settings = {
+    dots: false,
+    arrows: false,
+    infinite: true,
+    speed: 500,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+  };
+
   return (
     <>
       <div className="partner flex flex-col mx-auto lg:pb-24 lg:px-0 xs:pb-9 xs:px-6">
@@ -11,16 +35,56 @@ function Partner() {
           <h2 className=" font-bold lg:text-3xl xs:text-2xl text-center">
             Our Partner
           </h2>
-          <div className="partner-list flex items-start lg:gap-6 xs:gap-4 overflow-x-auto">
-            <div className="partner1 flex-shrink-0 flex w-44 h-16 content-center items-center">
-              <img src={Partner1} alt="partnerlogo" className="w-full h-auto" />
-            </div>
-            <div className="partner2 flex-shrink-0 flex w-44 h-16 content-center items-center">
-              <img src={Partner2} alt="partnerlogo" className="w-full h-auto" />
-            </div>
-            <div className="partner3 flex-shrink-0 flex w-44 h-16 content-center items-center">
-              <img src={Partner3} alt="partnerlogo" className="w-full h-auto" />
-            </div>
+          <div className="partner-list lg:gap-6 xs:gap-4 lg:flex">
+            {isMobile ? (
+              <Slider {...settings}>
+                <div className="partner1 flex content-center items-center">
+                  <img
+                    src={Partner1}
+                    alt="partnerlogo"
+                    className="w-full h-auto"
+                  />
+                </div>
+                <div className="partner2 flex content-center items-center">
+                  <img
+                    src={Partner2}
+                    alt="partnerlogo"
+                    className="w-full h-auto"
+                  />
+                </div>
+                <div className="partner3 flex content-center items-center">
+                  <img
+                    src={Partner3}
+                    alt="partnerlogo"
+                    className="w-full h-auto"
+                  />
+                </div>
+              </Slider>
+            ) : (
+              <>
+                <div className="partner1">
+                  <img
+                    src={Partner1}
+                    alt="partnerlogo"
+                    className="w-full h-auto"
+                  />
+                </div>
+                <div className="partner2">
+                  <img
+                    src={Partner2}
+                    alt="partnerlogo"
+                    className="w-full h-auto"
+                  />
+                </div>
+                <div className="partner3">
+                  <img
+                    src={Partner3}
+                    alt="partnerlogo"
+                    className="w-full h-auto"
+                  />
+                </div>
+              </>
+            )}
           </div>
         </div>
       </div>
