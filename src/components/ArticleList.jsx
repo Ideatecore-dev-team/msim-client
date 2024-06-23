b85f511229cb0ea02d5f66ca41d3b7c01bfb9765
nhsltn <nhsltndev@gmail.com>
2024-06-23 13:17:09 +0700

    Handle Article Payload

--- src/components/ArticleList.jsx
+++ src/components/ArticleList.jsx
@@ -1,11 +1,18 @@
 /* eslint-disable react/prop-types */
-import React from "react";
+import React, { useState, useRef } from "react";
 import ArrowOrange from "../../public/images/mascot-icons/arrow-orange.png";
 import parse from "html-react-parser";
 import { Link } from "react-router-dom";
 import "./ArticleList.css";
+import { FaArrowLeft } from "react-icons/fa6";
+import { FaArrowRight } from "react-icons/fa6";
 
 function ArticleList({ searchTerm, articles, sortOrder, selectedCategories }) {
+  const [visibleArticlesCount, setVisibleArticlesCount] = useState(5);
+  const [currentPage, setCurrentPage] = useState(1);
+  const articlesPerPage = 10;
+  const articleListContainerRef = useRef(null);
+
   const filterAndSortArticles = () => {
     let filteredArticles = articles;
 
@@ -39,6 +46,20 @@ function ArticleList({ searchTerm, articles, sortOrder, selectedCategories }) {
     return filteredArticles;
   };
 
+  const handleLoadMore = () => {
+    setVisibleArticlesCount(articlesPerPage);
+  };
+
+  const handleNextPage = () => {
+    setCurrentPage(currentPage + 1);
+    articleListContainerRef.current.scrollIntoView({ behavior: "smooth" });
+  };
+
+  const handlePreviousPage = () => {
+    setCurrentPage(currentPage - 1);
+    articleListContainerRef.current.scrollIntoView({ behavior: "smooth" });
+  };
+
   const filteredArticles = filterAndSortArticles();
 
   const extractAndLimitContent = (htmlContent, limit) => {
@@ -56,12 +77,31 @@ function ArticleList({ searchTerm, articles, sortOrder, selectedCategories }) {
     return `<p>${firstParagraph}</p>`; // Return as a paragraph
   };
 
+  // menentukan article yang di render berdasarkan current page and visible count
+  const startIndex = (currentPage - 1) * articlesPerPage;
+  const endIndex =
+    currentPage === 1
+      ? Math.min(visibleArticlesCount, filteredArticles.length)
+      : startIndex + articlesPerPage;
+  const displayedArticles = filteredArticles.slice(startIndex, endIndex);
+
+  const showLoadMore =
+    currentPage === 1 &&
+    filteredArticles.length > visibleArticlesCount &&
+    visibleArticlesCount < articlesPerPage;
+  const showPagination =
+    currentPage > 1 ||
+    (currentPage === 1 && visibleArticlesCount === articlesPerPage);
+
   return (
     <div className="article-list-section bg-background flex flex-col items-center self-stretch py-14 lg:pb-24 lg:pt-12">
-      <div className="article-lists-container flex flex-col items-center gap-7 lg:gap-10">
-        <h1 className=" headline-1">Article Terbaru</h1>
-        <div className="article-list flex flex-col items-start gap-4 lg:gap-6 ">
-          {filteredArticles.map((article, index) => (
+      <div
+        className="article-lists-container flex flex-col items-center gap-7 lg:gap-10"
+        ref={articleListContainerRef}
+      >
+        <h1 className="headline-1">Article Terbaru</h1>
+        <div className="article-list flex flex-col items-start gap-4 lg:gap-6">
+          {displayedArticles.map((article, index) => (
             <div
               key={index}
               className="article-card flex flex-col lg:flex-row bg-white rounded-2xl pb-8 lg:pr-6 lg:pb-0 gap-4 lg:gap-6 overflow-hidden"
@@ -70,17 +110,17 @@ function ArticleList({ searchTerm, articles, sortOrder, selectedCategories }) {
                 className="article-list-img bg-cover lg:rounded-2xl lg:h-full"
                 style={{ backgroundImage: `url(${article.image_heading})` }}
               ></div>
-              <div className=" article-content w-full lg:w-3/4 flex flex-col py-0 px-6 lg:py-6 lg:px-0 items-start gap-4">
-                <h4 className=" headline-4">{article.title}</h4>
+              <div className="article-content w-full lg:w-3/4 flex flex-col py-0 px-6 lg:py-6 lg:px-0 items-start gap-4">
+                <h4 className="headline-4">{article.title}</h4>
                 <div className="article-desc">
-                  <p className=" paragraph ">
+                  <p className="paragraph">
                     {parse(extractAndLimitContent(article.content, 120))}
                   </p>
                 </div>
-                <div className=" article-cta flex flex-col lg:flex-row items-center mx-auto gap-2 lg:gap-4 w-full">
+                <div className="article-cta flex flex-col lg:flex-row items-center mx-auto gap-2 lg:gap-4 w-full">
                   <p
                     style={{ backgroundColor: `${article.category_color}` }}
-                    className={`px-3 py-1 text-white w-28 text-center rounded-full`}
+                    className="px-3 py-1 text-white w-28 text-center rounded-full"
                   >
                     {article.category_name}
                   </p>
@@ -90,7 +130,7 @@ function ArticleList({ searchTerm, articles, sortOrder, selectedCategories }) {
                   >
                     Baca Selengkapnya
                     <img
-                      className="size-4 lg:size-6 "
+                      className="size-4 lg:size-6"
                       src={ArrowOrange}
                       alt=""
                     />
@@ -100,6 +140,37 @@ function ArticleList({ searchTerm, articles, sortOrder, selectedCategories }) {
             </div>
           ))}
         </div>
+        {showLoadMore && (
+          <button
+            onClick={handleLoadMore}
+            className="load-more-button flex items-center content-center gap-2 px-5 py-4 bg-primary-1 hover:bg-primary-2 text-white rounded-lg"
+          >
+            <p>Load More</p>
+            <FaArrowRight />
+          </button>
+        )}
+        {showPagination && (
+          <div className="pagination-buttons flex justify-between w-full px-4">
+            {currentPage > 1 && (
+              <button
+                onClick={handlePreviousPage}
+                className="previous-button flex items-center content-center gap-2 px-4 py-2 bg-primary-1 hover:bg-primary-2 text-white rounded-full"
+              >
+                <p>Previous</p>
+                <FaArrowLeft />
+              </button>
+            )}
+            {endIndex < filteredArticles.length && (
+              <button
+                onClick={handleNextPage}
+                className="next-button flex items-center content-center gap-2 px-4 py-2 bg-primary-1 hover:bg-primary-2  text-white rounded-full"
+              >
+                <p>Next</p>
+                <FaArrowRight />
+              </button>
+            )}
+          </div>
+        )}
       </div>
     </div>
   );
